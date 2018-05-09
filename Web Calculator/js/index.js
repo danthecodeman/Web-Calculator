9f2ba03e356540d7a4ce4239116ec8102f19aa62
danthecodeman <dmccollum@westmont.edu>
2018-05-09 11:15:37 -0700

    Update index.js

--- Web Calculator/js/index.js
+++ Web Calculator/js/index.js
@@ -55,6 +55,3 @@ $(function(){
     }
   });
 });
-// I can add, subtract, multiply and divide two numbers.
-// I can clear the input field with a clear button.
-// I can keep chaining mathematical operations together until I hit the equal button, and the calculator will tell me the correct output.
